c2d38646ddfa90082df0e875f387ca4d6b89575d
Rameshkartik <rameshkartik>
2017-06-07 18:22:51 +0530

    First Commit

---
+++ Angular2DI/Angular2DI/Binder/EmployeeModuleLibrary.ts
@@ -0,0 +1,21 @@
+﻿import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { EmployeeComponent } from '../Binder/EmployeeComponent';
+import { GridComponent } from './GridComponent';
+import { RandomComponent } from './RandomComponent';
+import { DateComponent } from './DateComponent';
+
+//const randomFactory = () => { return Math.random(); };
+
+@NgModule(
+    {
+        imports: [BrowserModule, FormsModule],
+        declarations: [EmployeeComponent, GridComponent, RandomComponent,DateComponent],
+        providers: [
+            { provide: 'Random', useValue: Math.random() },
+        ],
+        bootstrap: [EmployeeComponent]
+    })
+export class EmployeeModuleLibrary {
+}
